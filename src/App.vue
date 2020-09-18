53c92a9a31d5ba021b0c48d09092bff5a0f866a4
khalidamairi3 <khalidamairi3@gmail.com>
2020-09-17 20:23:20 -0600

    components data binding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button id="switch" @click="isGrid = !isGrid"> style switch</button>
+    <div id="tweets" v-bind:class="{gridTweets:isGrid}">
+      <pageTweet content="This is the first tweet on vue" date="17/09/2020" user="khaled" />
+      <pageTweet content="This is the second tweet on vue" date="17/09/2020" user="rainer" />
+      <pageTweet content="This is the third tweet on vue" date="17/09/2020" user="alex" />
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pageTweet from './components/tweet.vue' 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pageTweet
+  },
+  data:function() {
+    return{
+    isGrid:false
+  }
   }
 }
 </script>
@@ -24,5 +34,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+}
+
+.gridTweets{
+  display: grid;
+  grid-template-columns: repeat(3,1fr) ;
+  align-items: center;
+  justify-items: center;
+}
+#tweets{
+  display: grid;
+  align-items: center;
+  justify-items: center;
+}
+#switch{
+  background-color: green;
+  color: white;
+  width: 200px;
+  height: 50px;
 }
 </style>
---
+++ src/components/tweet.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="tweet">
+        <h1> {{ user }}</h1>
+        <h6> {{date}} </h6>
+        <h4 > {{ content}}</h4>
+        <h2 @click="like = true" v-if="!like"> Like</h2>
+        <h2 v-else> unlike</h2>
+
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "page-tweet",
+    props:{
+        user :{
+         type:String,
+         required: true,
+         default: "user",
+         
+        },
+        content:String,
+        date:String,
+
+    },
+    data: function(){
+        return{
+            like : false,
+
+        }
+
+    }
+}
+
+</script>
+
+<style  scoped>
+
+#tweet{
+    width: 50%;
+    border: 1px solid black;
+    border-radius: 15px 15px 15px 15px; 
+    
+}
+</style>
